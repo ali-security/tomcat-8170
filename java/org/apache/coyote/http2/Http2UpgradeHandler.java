40676d3527663f1637b18cf78f574ff24dc99952
Tal Samucha <tal.s@sealsecurity.io>
2025-08-25 15:14:09 +0300

    CVE-2023-44487

--- java/org/apache/coyote/http2/Http2UpgradeHandler.java
+++ java/org/apache/coyote/http2/Http2UpgradeHandler.java
@@ -375,7 +375,7 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
                                     stream.close(se);
                                 }
                             } finally {
-                                if (overheadCount.get() > 0) {
+                                if (isOverheadLimitExceeded()) {
                                     throw new ConnectionException(
                                             sm.getString("upgradeHandler.tooMuchOverhead", connectionId),
                                             Http2Error.ENHANCE_YOUR_CALM);
@@ -1503,6 +1503,9 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
         }
     }
 
+    boolean isOverheadLimitExceeded() {
+        return overheadCount.get() > 0;
+    }
 
     // ----------------------------------------------- Http2Parser.Input methods
 
@@ -1591,6 +1594,7 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
             }
         }
 
+        increaseOverheadCount(FrameType.RST, getProtocol().getOverheadResetFactor());
         AbstractNonZeroStream abstractNonZeroStream = getAbstractNonZeroStream(streamId, true);
         abstractNonZeroStream.checkState(FrameType.DATA);
         abstractNonZeroStream.receivedData(payloadSize);
